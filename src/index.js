3a3befcc2b4449344a0d2a5a7fe2f804dd2984c4
v_yguiliu <v_yguiliu@tencent.com>
2019-01-02 17:58:54 +0800

    feat: 完成最基础css加载功能

--- src/index.js
+++ src/index.js
@@ -3,78 +3,83 @@ import fs from 'fs'
 import mkdirp from 'mkdirp'
 
 import { createFilter } from 'rollup-pluginutils'
-console.log(process.env.NODE_ENV)
-export default function plugin (options) {
-  options = options || {
-    include: ['**/*.css'],
-    exclude: []
+
+function compileStyle (code, type) {
+  return code
+}
+
+function joinStyle (list) {
+  const styleList = []
+
+  for (let id in list) {
+    list[id].code && styleList.push(compileStyle(list[id].code, list[id].extname))
   }
+
+  if (styleList.length === 0) return null
+
+  return styleList.join('')
+}
+
+export default function plugin (options = {}) {
+  options.include = options.include || ['**/*.css']
+  options.exclude = options.exclude || []
+  options.output = options.output || 'inline' // style, file, inline
+
   const filter = createFilter(options.include, options.exclude)
   const styles = {}
 
   return {
     name: 'style',
     intro () {
-      console.log('intro')
-      return '// intro'
-    },
-    outro () {
-      console.log('outro', styles)
-      return '// outro'
+      if (options.output !== 'style') return null
+
+      const styleText = joinStyle(styles)
+
+      return `function __insertStyle (css) {
+        const style = document.createElement('style')
+        style.innerHTML = css
+        style.type = 'text/css'
+        document.head.appendChild(style)
+      };
+      __insertStyle(${JSON.stringify(styleText)});
+      `
     },
     transform (code, id) {
       if (!filter(id)) return
-      // const extname = path.extname(id)
-
-      console.log('transform', id)
-      if (options.output === false) {
+      const extname = path.extname(id)
+      if (options.output === 'inline') {
         return {
-          code: 'export default ' + JSON.stringify(code),
+          code: 'export default ' + JSON.stringify(compileStyle(code, extname)),
           map: { mappings: '' }
         }
       }
 
       if (styles[id] !== code) {
-        styles[id] = code
+        styles[id] = {
+          extname,
+          code
+        }
       }
       // 返回空字符串
       return ''
     },
     generateBundle (opts, bundle, isWrite) {
-      console.log('generateBundle')
-      console.log(opts, bundle)
-
-      if (options.output === false) {
+      if (options.output === 'inline') {
         return null
       }
 
-      let entry = null
+      const styleText = joinStyle(styles)
 
-      for (let key in bundle) {
-        if (bundle[key].isEntry) {
-          entry = bundle[key]
-        }
-      }
+      if (options.output !== 'file') return null
 
-      if (!entry) return null
-
-      const styleList = []
-      for (let id in entry.modules) {
-        if (filter(id)) {
-          if (styles[id]) styleList.push(styles[id])
-        }
-      }
-
-      console.log(styleList)
-
-      const filePath = typeof options.output === 'string' ? options.output : opts.file.replace(path.extname(opts.file), '.css')
+      const filePath = options.dest ? options.dest : opts.file.replace(path.extname(opts.file), '.css')
       const dir = path.dirname(filePath)
 
       return new Promise(function (resolve, reject) {
         mkdirp(dir, function (err) {
           if (err) reject(err)
 
-          fs.writeFile(filePath, styleList.join(''), function (err) {
+          fs.writeFile(filePath, styleText, function (err) {
             if (err) reject(err)
 
             resolve()

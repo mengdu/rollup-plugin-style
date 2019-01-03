c248b7887d57e1141aa23acdaaf105f481e3d8d2
mengdu <1752295326@qq.com>
2019-01-03 22:46:29 +0800

    feat: 添加 options.compile 支持

--- src/index.js
+++ src/index.js
@@ -4,50 +4,52 @@ import mkdirp from 'mkdirp'
 
 import { createFilter } from 'rollup-pluginutils'
 
-function compileStyle (code, type) {
-  return code
-}
-
-function joinStyle (list) {
-  const styleList = []
-
-  for (let id in list) {
-    list[id].code && styleList.push(compileStyle(list[id].code, list[id].extname))
-  }
-
-  return styleList.join('')
-}
-
 export default function plugin (options = {}) {
   options.include = options.include || ['**/*.css']
   options.exclude = options.exclude || []
   options.output = options.output || 'inline' // style, file, inline
+  options.compile = options.compile || async function (code, type) {
+    return code
+  }
+
+  async function joinStyle (list) {
+    const styleList = []
+
+    for (let id in list) {
+      list[id].code && styleList.push(await options.compile(list[id].code, list[id].extname))
+    }
+
+    return styleList.join('')
+  }
 
   const filter = createFilter(options.include, options.exclude)
   const styles = {}
 
   return {
     name: 'style',
-    intro () {
+    async intro () {
       if (options.output !== 'style') return null
 
-      const styleText = joinStyle(styles)
+      const styleText = await joinStyle(styles)
 
-      return `function __insertStyle (css) {
-        const style = document.createElement('style')
-        style.innerHTML = css
-        style.type = 'text/css'
-        document.head.appendChild(style)
-      };
-      __insertStyle(${JSON.stringify(styleText)});
+      return `
+        ;(function () {
+          function __insertStyle (css) {
+            const style = document.createElement('style')
+            style.innerHTML = css
+            style.type = 'text/css'
+            document.head.appendChild(style)
+          };
+          __insertStyle(${JSON.stringify(styleText)});
+        })();
       `
     },
-    transform (code, id) {
+    async transform (code, id) {
       if (!filter(id)) return
       const extname = path.extname(id)
       if (options.output === 'inline') {
         return {
-          code: 'export default ' + JSON.stringify(compileStyle(code, extname)),
+          code: 'export default ' + JSON.stringify(await options.compile(code, extname)),
           map: { mappings: '' }
         }
       }
@@ -61,12 +63,12 @@ export default function plugin (options = {}) {
       // 返回空字符串
       return ''
     },
-    generateBundle (opts, bundle, isWrite) {
+    async generateBundle (opts, bundle, isWrite) {
       if (options.output === 'inline') {
         return null
       }
 
-      const styleText = joinStyle(styles)
+      const styleText = await joinStyle(styles)
 
       if (options.output !== 'file') return null
 
